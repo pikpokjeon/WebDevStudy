379519e24ff51a0fe50cfb6c0fccc71cc7aa3303
YaejinJeon <pikpokjeon@gmail.com>
2020-08-09 10:32:45 +0900

    canvasJS drawing rectangle triangle lines with path

---
+++ webDevelopment/Javascript/canvas.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+  const canvas = document.getElementById("newCanvas");
+  const ctx = canvas.getContext("2d"); //3차원 WebGl는 'webgl'
+
+  //그리기
+
+
+  //
+
+  // 좌표 (50,50)에 너비가 200이고 높이가  100인 사각형 테두리
+  //ctx (context) 
+  ctx.strokeRect(50,50,200,140);
+
+  // 사각형 채우기
+  ctx.fillRect(400,250,300,300);
+
+  //사각형 영역을 지우고 투명하게
+  ctx.clearRect(450,300,150,150);
+
+  //삼각형 채우기
+  ctx.beginPath();
+  ctx.moveTo(160,250); //좌표로 이동
+  ctx.lineTo(110,220); //현재 좌표에서 지정된 좌표까지 이동
+  ctx.lineTo(10,210);
+  // 패스의 마지막 점과 시작점을 직선으로 연결하고 패스닫음
+  ctx.closePath();
+ //삼각형 테두리를 그릴 때는 closepath로 패스를 닫아야함
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(300,200);
+  ctx.lineTo(10,200);
+  ctx.lineTo(10,100);
+  // fill메서드는 자동으로 패스를 닫아줌
+  ctx.fill();
+
+  //연결되지 않은 패스 정의
+  ctx.beginPath();
+  ctx.moveTo(150,600);
+  ctx.lineTo(150,800);
+  ctx.moveTo(90,400);
+  ctx.lineTo(150,600);
+  ctx.stroke();
+
+
+}

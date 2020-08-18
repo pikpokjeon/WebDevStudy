e9d953c3ece9b623a2a49540ffa3e41e97c9df42
YaejinJeon <pikpokjeon@gmail.com>
2020-08-18 23:28:00 +0900

    make the app only returns new added letters after 3 seconds when theres no changes

--- webDevelopment/react-study/src/App.js
+++ webDevelopment/react-study/src/App.js
@@ -1,61 +1,44 @@
 import React, { useState, useEffect } from "react";
-import FocusableInput from "./input";
-// import "./styles.css";
+import FocusableInput from "./FocusableInput";
+import "./styles.css";
 
 const App = () => {
   const [focused, setFocused] = useState(false);
   const [word, setWord] = useState("");
-  const [added, setAdded] = useState("");
-  let newTyped;
-  let timer;
+  const [timer, setTimer] = useState(undefined);
 
   const handleChangeFocused = (e) => {
     setFocused(!focused);
-  };
+};
 
   useEffect(() => {
     // console.log("> Checkbox >", focused);
-    // console.log("> WordState >",word);
   });
 
   const handleChangeInput = (e) => {
     console.log("-----OnChange Start-------");
-    console.log(">-----newtyping-------<");
+    console.log(">> newtyping <<");
 
     e.persist();
-    setWord(e.target.value);
-    newTyped = e.target.value;
-    // console.log("newTyped1", newTyped);
-    console.log("word1", word);
-    setAdded(newTyped.substring(word.length, newTyped.length));
 
-    console.log("Added1", added);
-    // if (timer) {
-    //   clearTimeout(timer);
-    // }
-    timer = setTimeout(() => {
+    const content = e.target.value;
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    let newTimer = setTimeout(() => {
       console.log("-----setTimeout Start-------");
-      // let newTypedAfter = e.target.value;
-      setWord(e.target.value);
-      setAdded(newTyped.substring(word.length, newTyped.length));
+      const added = content.substr(word.length);
+      setWord(content);
       console.log("Added>", added);
 
-      newTyped = e.target.value;
+      setTimer(undefined);
 
-      console.log("2sec passed--------");
-      // console.log("newTyped2", newTyped);
-      console.log("word2", word);
-      if (newTyped.indexOf(word.valueOf)) {
-        let newAdded = newTyped.substring(word.length, newTyped.length);
-        console.log("Added2", added);
-        console.log("New added after 3 sec >", newAdded);
-      } else {
-        console.log("Cannot compare.");
-      }
-      clearTimeout(timer);
-      console.log("-----clearTimeoutt-------");
+      console.log("/---clearTimeout----/");
     }, 3000);
     console.log("-----OnChange Ends-------");
+    setTimer(newTimer);
   };
 
   return (

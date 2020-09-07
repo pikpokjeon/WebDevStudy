f86068fcb1ddc64e42a486812987566a62587825
YaejinJeon <pikpokjeon@gmail.com>
2020-09-07 23:10:22 +0900

    Vue TodoList completing Input template

--- webDevelopment/VueJS/vue-cli/src/components/TodoInput.vue
+++ webDevelopment/VueJS/vue-cli/src/components/TodoInput.vue
@@ -6,8 +6,8 @@
     <button class="addContainer" v-on:click="addTodo">
       <!-- <font-awesome icon="fa-plus" /> -->
      push
-      </button>
-
+    </button>
+     
       <Modal v-if="showModal" @close="showModal = false">
 
         <h3 slot="header">확인</h3>
@@ -15,13 +15,14 @@
         <small slot="footer" @click="showModal = false">끝</small>
       </Modal>
 
-
   </div>
 </template>
 
 <script>
 import Modal from './common/Modal.vue';
+
 export default {
+
   data: function(){
     return {
       newTodoItem: "",
@@ -32,25 +33,51 @@ export default {
     addTodo: function(){
       if ( this.newTodoItem !==''){
         // this.$emit('addTodoItem' ,this.newTodoItem);
-        this.$store.commit('addOneItem', this.newTodoItem);//mutation 동작을 위해
+        this.$store.commit('addOneItem', this.newTodoItem);//store의 mutation을 실행 
         this.clearInput();
+
       } else {
         this.showModal = !this.showModal;
       }
     
-  },
+     },
     clearInput: function(){
       this.newTodoItem = '' ;
 
     }
   },
   components: {
-    Modal : Modal,
+     Modal,
   }
-
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
+}
 </style>
